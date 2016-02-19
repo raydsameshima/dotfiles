0f84d75b2b046d6c3fbaf03a28f603935acf8f62
raydsameshima <rsameshima@citytech.cuny.edu>
2016-02-19 00:14:57 -0500

    correct the path: maxima-init

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,4 @@ ln -sf ~/.files/.vimrc ~/.vimrc
 ln -sf ~/.files/.bash_profile ~/.bash_profile
 ln -sf ~/.files/.ghci ~/.ghci          
 ln -sf ~/.files/.vimrc ~/.vimrc         
-ln -sf ~/.files/.maxima/maxima-init.mac ~/.maxima/maxima-init.mac
+ln -sf ~/.files/maxima-init.mac ~/.maxima/maxima-init.mac
