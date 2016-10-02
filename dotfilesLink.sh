9f88215078dbdf900c4674cecc9bd722cd8a43a8
Ray Sameshima <raysameshima@rds.local>
2016-10-02 11:44:30 -0400

    files to dotfiles

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-ln -sf ~/.files/.vimrc ~/.vimrc
-ln -sf ~/.files/.bash_profile ~/.bash_profile
-ln -sf ~/.files/.ghci ~/.ghci          
-ln -sf ~/.files/.vimrc ~/.vimrc         
-ln -sf ~/.files/maxima-init.mac ~/.maxima/maxima-init.mac
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/.dotfiles/.ghci ~/.ghci          
+ln -sf ~/.dotfiles/.vimrc ~/.vimrc         
+ln -sf ~/.dotfiles/maxima-init.mac ~/.maxima/maxima-init.mac
