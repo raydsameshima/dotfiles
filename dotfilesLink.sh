517617981164a463ea3224575aacc0dba45ea26d
Ray D. Sameshima <rds@rds.local>
2016-11-17 18:25:33 -0500

    add mit-scheme+bindings

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/.dotfiles/.ghci ~/.ghci          
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc         
 ln -sf ~/.dotfiles/maxima-init.mac ~/.maxima/maxima-init.mac
+ln -sf ~/.dotfiles/.mit_scheme_bindings.txt ~/.mit_scheme_bindings.txt         
